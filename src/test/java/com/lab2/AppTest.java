18aca330256926e208172750307e47e966c4a22e
Egor Ovsyannikov <e.a.ovsya@gmail.com>
2019-06-27 01:46:58 +0300

    lab2 test fixed

--- src/test/java/com/lab2/AppTest.java
+++ src/test/java/com/lab2/AppTest.java
@@ -1,20 +1,21 @@
 package com.lab2;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 
 public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() throws IOException {
+        File file = new File("testFile.txt");
         String expectedInfo = "testFile.txt\r\n" +
-                "111 Wed Jun 26 14:54:52 MSK 2019 0\r\n" +
+                "111" + " " + new Date(file.lastModified()) + " " + "0\r\n" +
                 "rwx 0 bytes\r\n";
         String[] arguments = { "-l", "-h", "-r", "-o", "outputTest.txt", "testFile.txt"};
         new fileProcessorLauncher().launch(arguments);
